2d450f786a08e0199fd174308c861a32b28879e9
matt-davz <davies.matt.02@gmail.com>
2023-10-25 17:22:38 +0700

    testing for percision option done

---
+++ test/boi.js
@@ -0,0 +1,4 @@
+const testNum = 1234567891;
+
+
+console.log((testNum/Math.pow(10,10-1)).toFixed(1))
--- test/prettyDigits.test.js
+++ test/prettyDigits.test.js
@@ -1,5 +1,11 @@
 const {prettyDigits} = require('../lib/prettyDigits.cjs')
 
 test('Testing percision option to 10 decimal place', () => {
-    expect(prettyDigits(1234567,{percision: 1})).toBe('1.2 M')
+    const testNum = 1234567891;
+    let expectedNum = ['1.2 B','1.23 B','1.235 B','1.2346 B','1.23457 B','1.234568 B', '1.2345679 B','1.23456789 B', '1.234567891 B']
+    for(let i = 1; i <10; i++){
+        expect(prettyDigits(testNum,{percision: i})).toBe(`${expectedNum[i-1]}`)
+    }
+    
 }) 
+
