b7c11861d0f25e01e49c35b2bf87ba564e155b9d
matt-davz <davies.matt.02@gmail.com>
2023-10-25 23:05:53 +0700

    works for numbers under 100 and the options work

--- test/prettyDigits.test.js
+++ test/prettyDigits.test.js
@@ -3,7 +3,7 @@ const {prettyDigits} = require('../lib/prettyDigits.cjs')
 test('Testing percision option to 10 decimal place', () => {
     const testNum = 1234567891;
     let expectedNum = ['1.2 B','1.23 B','1.235 B','1.2346 B','1.23457 B','1.234568 B', '1.2345679 B','1.23456789 B', '1.234567891 B']
-    for(let i = 1; i <10; i++){
+    for(let i = 1; i < expectedNum.length; i++){
         expect(prettyDigits(testNum,{percision: i})).toBe(`${expectedNum[i-1]}`)
     }
     
@@ -14,7 +14,31 @@ test('Can use custom units and devisor will increase automatically', () => {
     const testUnits = ['','b','kb','mb','gb','tb','pb']
     const expectedOutputs = ['1 b','1 kb','1 mb','1 gb','1 tb','1 pb']
 
-    for(let i = 0; i < testNums.length; i++){
+    testNums.forEach((elm,i) => {
         expect(prettyDigits(testNums[i],{units: testUnits})).toBe(expectedOutputs[i])
-    }
+    })
+  
+})
+
+describe('testing numbers under 100', () => {
+    test('still have precision work with their decimals', () => {
+        const testNums = [500,500.12,500.12,10.005]
+        const testOptions = [{precision: 0},{precision: 1},{precision: 2},{precision: 3}]
+        const expectedOutputs = ['500 ','500.1 ','500.12 ','10.005 ']
+        
+        testNums.forEach((elm,i) => {
+            expect(prettyDigits(testNums[i],testOptions[i])).toBe(expectedOutputs[i])
+        })
+    })
+
+    test('still work with custom units', () => {
+        const testNums = [300,999,1,10.5];
+        const testUnits = ['b'];
+        const expectedOutputs = ['300 b','999 b','1 b','11 b'];
+
+        testNums.forEach((elm,i) => {
+            expect(prettyDigits(testNums[i],{units: testUnits})).toBe(expectedOutputs[i])
+        })
+
+    }) 
 })
